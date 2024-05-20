a6d3919ce6d5f6ecf100f1efd49e9076e4a4cf36
leonelpucineli <leonelpucineli@gmail.com>
2024-05-19 21:31:55 -0300

    Y Sort: fake 3d efect based on y axe.

---
+++ codigo.py
@@ -0,0 +1,81 @@
+import pygame, sys
+from random import randint
+
+class Tree(pygame.sprite.Sprite):
+    def __init__(self,pos,group):
+        super().__init__(group)
+        self.image = pygame.image.load('graphics/tree.png').convert_alpha()
+        self.rect = self.image.get_rect(topleft=pos)
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self,pos,group):
+        super().__init__(group)
+        self.image = pygame.image.load('graphics/player.png').convert_alpha()
+        self.rect = self.image.get_rect(center=pos)
+        self.direction = pygame.math.Vector2()
+        self.speed = 5
+
+    # o que seria, em outros casos, um método move(), nesse caso, está dividido em input() e update()
+    def input(self):
+        pressed_key = pygame.key.get_pressed()
+
+        # Executa os movimentos dos players
+        if pressed_key[pygame.K_UP]:
+            self.direction.y = -1
+        elif pressed_key[pygame.K_DOWN]:
+            self.direction.y = 1
+        else:
+            self.direction.y = 0
+
+        if pressed_key[pygame.K_RIGHT]:
+            self.direction.x = 1
+        elif pressed_key[pygame.K_LEFT]:
+            self.direction.x = -1
+        else:
+            self.direction.x = 0
+
+    def update(self):
+        self.input()
+        self.rect.center += self.direction * self.speed
+
+class CameraGroup(pygame.sprite.Group):
+    def __init__(self):
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+        self.ground_surf = pygame.image.load('graphics/ground.png').convert_alpha()
+        self.ground_rect = self.ground_surf.get_rect(topleft=(0, 0))
+
+    def custom_draw(self):
+        # ground
+        self.display_surface.blit(self.ground_surf,self.ground_rect)
+
+        # active elementes
+        for sprite in sorted(self.sprites(), key=lambda sprite: sprite.rect.centery):
+            self.display_surface.blit(sprite.image, sprite.rect)
+
+pygame.init()
+window = pygame.display.set_mode((1280,720))
+clock = pygame.time.Clock()
+
+# setup
+camera_group = CameraGroup()
+Player((640,360),camera_group)
+
+for i in range(20):   
+    random_x = randint(0,1000)
+    random_y = randint(0,1000)
+    Tree((random_x,random_y),camera_group)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        
+    window.fill('#71ddee')
+
+    camera_group.update()
+    camera_group.custom_draw()
+
+    pygame.display.update()
+    clock.tick(30)
